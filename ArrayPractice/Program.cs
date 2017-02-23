008f7d7ff200de35590b742aa5d5f29d2698fac2
Carrie Ryan <cryan1205@gmail.com>
2017-02-22 20:22:03 -0500

    ArrayPractice
    Practice using arrays in C#

---
+++ ArrayPractice/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfTheWeek = {"Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday","Sunday"};
+
+            Console.WriteLine(daysOfTheWeek[2]);
+        }
+    }
+}
+*/
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //program to enter student ages into array
+
+            int[] studentAges = new int[6];
+
+            Console.WriteLine("Enter student 1's age:");
+            studentAges[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student 2's age:");
+            studentAges[1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student 3's age:");
+            studentAges[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student 4's age:");
+            studentAges[3] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student 5's age:");
+            studentAges[4] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter student 6's age:");
+            studentAges[5] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the number of students included in this array is " + studentAges.Length);
+
+            foreach (int age in studentAges)
+            {
+                //loop in body. Print numbers
+                Console.WriteLine(age);
+                
+            }
+
+        }
+    }
+}
+*/
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Declare and initialize an array of characters spelling out your last name. Print the length of the array.
+
+        char[] lastName = { 'R', 'Y', 'A', 'N' };
+
+        Console.WriteLine("there are " + lastName.Length + " letters in this array");
+
+            Console.WriteLine(lastName);
+    }
+}
+}
+
+*/
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //program to enter student ages into array
+
+        string[] studentName = { "Carrie", "dog", "Javi" };
+
+            Console.WriteLine(studentName[2]);
+
+           
+
+
+
+    }
+}
+}
+*/
+
+
+/* using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //lucky number array
+
+            int[] luckyNumbers = { 8, 4, 1, 13, 27, 37, 38, 8, 4, 1, 13, 27, 37, 38 };
+
+            Console.WriteLine(Array.LastIndexOf(luckyNumbers, 38));
+
+                }
+
+    }
+}  
+
+*/     
+
